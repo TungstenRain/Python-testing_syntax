c67b0a0ec1c29a7b53a1444facc25691242ee8fc
Frank Olson <frank.olson@gmail.com>
2020-09-30 12:28:53 -0500

    Updated header information

--- syntax_test.py
+++ syntax_test.py
@@ -4,9 +4,6 @@
     by Allen Downey
     http://thinkpython2.com
 
-    Copyright 2015 Allen B. Downey
-    License: GNU GPLv3 http://www.gnu.org/licenses/gpl.html
-    
     This is to try out syntax errors and determine how the interpreter will handle them
     
     Note: Although this is saved in a .py file, code was run on an interpreter to get results
